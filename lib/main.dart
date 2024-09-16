f7ac5a3f11303cfe3c5df1cedbd663c4276a9401
FelixPrajna <felix.535230121@stu.untar.ac.id>
2024-09-16 22:45:58 +0700

    setup untuk menyambungkan flutter dengan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ojolali/authentication/login_screen.dart';
 import 'package:ojolali/authentication/signup_screen.dart';
 
-void main() {
+Future<void> main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
