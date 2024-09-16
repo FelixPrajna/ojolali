1600536cbff0a3fb887704719a95a5222dfc0c26
Fawaz8581 <ffawaz8581@gmail.com>
2024-09-16 21:04:01 +0700

    Membuat Login dan Sign up Screen

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,17 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:ojolali/authentication/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State LoginScreen> createState() =>  LoginScreenState();
+  State<LoginScreen> createState() => LoginScreenState();
 }
 
-class  LoginScreenState extends State LoginScreen> 
-{
+class LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
-  Widget build(BuildContext context) 
-  {
-    return const Placeholder();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset("images/logo.png"), // buat gambar
+
+              const Text(
+                "Login as a User",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(22),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 80, vertical: 10)),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (c) => SignupScreen()));
+                        },
+                        child: Text(
+                          "Dont 't have an Account? Register Here",
+                          style: TextStyle(color: Colors.grey),
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -1,27 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:ojolali/authentication/login_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
 
   @override
-  State SignupScreen> createState() =>  SignupScreenState();
+  State<SignupScreen> createState() => SignupScreenState();
 }
 
-class  SignupScreenState extends State SignupScreen> 
-{
+class SignupScreenState extends State<SignupScreen> {
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          Children: (
-            
-          )
-        )
-      )
-    )
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset("images/logo.png"), // buat gambar
+
+              const Text(
+                "Create a user 's Account",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(22),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: userNameTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Name",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 80, vertical: 10)),
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (c) => LoginScreen()));
+                        },
+                        child: Text(
+                          "Already have an Account? Login Here",
+                          style: TextStyle(color: Colors.grey),
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
