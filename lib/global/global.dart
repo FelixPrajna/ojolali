1e4adf13db85ab54a1f83b724ebcff5ab7d88c3d
KuRiMuYuu <william.535230040@stu.untar.ac.id>
2024-10-04 14:37:44 +0700

    mengganti style dan theme di Google map

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,8 +1,9 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 String userName = "";
 String googleMapKey = "AIzaSyA9VHnA4yqys2vddb9xlUgrJTKQchPGdNI";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
+  target: LatLng(-6.2087634, 106.845599),
   zoom: 14.4746,
 );
